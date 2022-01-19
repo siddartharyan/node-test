523624b686cca0da20c1408a45c1c359b9a39286
sai.v <sai.v@rakuten.com>
2022-01-19 15:05:09 +0530

    initial

---
+++ index.js
@@ -0,0 +1,15 @@
+const app=require('express')()
+const cors=require('cors')
+const requestIp = require('request-ip');
+app.use(cors())
+
+app.get("/check/api",(req,res)=>{
+    res.json({"ip":req.socket.remoteAddress})
+})
+
+app.get("/check/ip",(req,res)=>{
+    const ip=requestIp.getClientIp(req)
+    res.json({"ip_ip":ip})
+})
+
+app.listen(8000,console.log('server started'))
